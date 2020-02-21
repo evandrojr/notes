d3fa809e16611c22b1ea5d311f2c1c549cc75c91
Evandro Junior <evandrojr@gmail.com>
2020-02-20 22:29:54 -0300

    Minor changes

--- notes.py
+++ notes.py
@@ -39,14 +39,14 @@ imgicon = tk.PhotoImage(file=icon_path)
 root.tk.call('wm', 'iconphoto', root._w, imgicon)  
 root.title('Anotações do Dengo')
 label = tk.Label(root, anchor="w")
-text = CustomText(root, width=40, height=4, font=('Courier',16), fg='#FFFFFF', bg='#002B36', insertbackground='white' )
+text = CustomText(root, width=40, height=4, font=('Courier',16), fg='white', bg='#002B36', insertbackground='white' )
 
 if os.path.exists(storage_path):
     text.insert(tk.END, open(storage_path).read())
 
 label.pack(side="bottom", fill="x")
 text.pack(side="top", fill="both", expand=True)
-label.configure(text="Relaxe, mas não ao ponto de esquecer de ser feliz e bondoso!")
+label.configure(text="Relaxe e seja feliz, mas não ao ponto de se esquecer de ser uma boa pessoa!")
 
 def onModification(event):
     content = event.widget.get("1.0", "end-1c")
